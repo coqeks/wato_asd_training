aac6a551e81516eaa9ea984b41464d25588f8ed8
Jason <jason@eduroam-campus-10-36-76-177.campus-dynamic.uwaterloo.ca>
2026-09-18 15:56:37 -0400

    Updated Costmap node: iterate through ranges to mark o_grid cells, inflation not yet added.

--- src/robot/costmap/include/costmap_node.hpp
+++ src/robot/costmap/include/costmap_node.hpp
@@ -3,20 +3,31 @@
  
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
  
 #include "costmap_core.hpp"
- 
+
+// Declaring node class and its properties
+// CostmapNode class is inherited from rclcpp's 'Node' class - https://docs.ros2.org/foxy/api/rclcpp/classrclcpp_1_1Node.html
 class CostmapNode : public rclcpp::Node {
   public:
     CostmapNode();
     
     // Place callback function here
     void publishMessage();
+    void processLaserMsg(sensor_msgs::msg::LaserScan::SharedPtr); // Declare laser scan subscriber as 
+    std::vector<std::vector<int>> initGrid();
+    // Shared pointer instead of complete copy, this is the standard apparently
+
+    float grid_res = 0.1; // in meters
+    int grid_width = 10;
+    int grid_height = 10;
  
   private:
     robot::CostmapCore costmap_;
     // Place these constructs here
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr string_pub_;
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_scan_;
     rclcpp::TimerBase::SharedPtr timer_;
 };
  
--- src/robot/costmap/src/costmap_node.cpp
+++ src/robot/costmap/src/costmap_node.cpp
@@ -1,12 +1,20 @@
 #include <chrono>
 #include <memory>
+#include <array>
+#include <vector>
  
 #include "costmap_node.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
  
+// Initializer list (what's after :), Initializes Node() base class and costmap_ property before constructor body runs.
+// Convention is not to put subscription / publisher on those, but cheap / necessary member variables
 CostmapNode::CostmapNode() : Node("costmap"), costmap_(robot::CostmapCore(this->get_logger())) {
   // Initialize the constructs and their parameters
-  string_pub_ = this->create_publisher<std_msgs::msg::String>("/test_topic", 10);
+  string_pub_ = this->create_publisher<std_msgs::msg::String>("/test_topic", 10); 
   timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&CostmapNode::publishMessage, this));
+  // this = pointer to the object a function was called on
+  laser_scan_ = this->create_subscription<sensor_msgs::msg::LaserScan>("/lidar", 10, std::bind(&CostmapNode::processLaserMsg, this, std::placeholders::_1)); // Because readLaserMsg takes one argument, you need a placeholder 
+  // 
 }
  
 // Define the timer to publish a message every 500ms
@@ -16,11 +24,54 @@ void CostmapNode::publishMessage() {
   RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
   string_pub_->publish(message);
 }
- 
+
+std::vector<std::vector<int>> CostmapNode::initGrid() {
+  std::vector<std::vector<int>> grid(this->grid_width / this->grid_res, std::vector<int>(grid_height / this->grid_res, 0));
+  return grid;
+}
+
+// LaserScan:
+//  - angle_min and angle_max, with angle_increment
+//  - ranges (up to 256), distances ordered by angle (starting from min to max with increment of angle_increment)
+//  - See how many coordinates are in a grid, if none = zero chance of danger, if > 0 than increased chance
+//  - Discard and range in ranges, simple conditional check
+//  - Process:
+//    - Calculate x and y coordinate of each range items -> find the cell corresponding to the coordinates, mark as occupied (0 -> 100) -> return the o_grid
+//    - Grid increment = 0.1m
+//    - Grid size = 10m x 10m 
+void CostmapNode::processLaserMsg(sensor_msgs::msg::LaserScan::SharedPtr laserScan) {
+
+  std::vector<std::vector<int>> o_grid = initGrid();
+  float min_angle = laserScan->angle_min;
+
+  for (int i = 0; i < laserScan->ranges.size(); i++) {
+
+    // Make sure distance is not too small / big
+    if (laserScan->ranges[i] > laserScan->range_max || laserScan->ranges[i] < laserScan->range_min) {
+      continue;
+    }
+
+    float angle = min_angle + laserScan->angle_increment * i;
+    float x_dis = laserScan->ranges[i] * cos(angle);
+    float y_dis = laserScan->ranges[i] * sin(angle);
+    float grid_res = this->grid_res;
+    // static_cast to ensure what we get is an int
+    // displacement / grid resolution gives me displacement with respect to the grid's scale, 
+    // which i can add to the centre of axis to find the position in grid.
+    int x_grid = static_cast<int>(x_dis / grid_res) + grid_width / 2; 
+    int y_grid = static_cast<int>(y_dis / grid_res) + grid_height / 2;
+
+    // Make sure grid values are within the grid
+    if (x_grid < grid_width || x_grid >= 0 || y_grid < grid_height || y_grid >= 0) {
+      o_grid[y_grid][x_grid] = 100; // grid certainly has obstacle within
+    }
+  }
+}
+
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<CostmapNode>());
+  rclcpp::spin(std::make_shared<CostmapNode>()); // Blocks the thread to keep the node (Costmap) running, starts an event loop for subscription / timer 
   rclcpp::shutdown();
   return 0;
 }
